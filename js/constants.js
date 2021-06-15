cd8a195f44a2368a6b9f151c635c3e8ee2bdaa04
Pankaj Kargirwar <kargirwar@protonmail.com>
2021-06-15 10:45:16 +0530

    Testing canvas

---
+++ js/constants.js
@@ -0,0 +1,34 @@
+class Constants {
+    static get SESSION_ID() {
+        return 'session-id'
+    }
+
+    static get URL() {
+        return 'http://localhost:23890'
+    }
+
+    static get WS_URL() {
+        return 'ws://localhost:23890'
+    }
+
+    static get DB_NAME() {
+        return 'prosql'
+    }
+
+    static get DB_VERSION() {
+        return 1
+    }
+
+    static get CONNECTIONS() {
+        return 'connections'
+    }
+
+    static get BATCH_SIZE() {
+        return 1000
+    }
+
+    static get CREDS() {
+        return 'creds'
+    }
+}
+export { Constants }
--- js/index.js
+++ js/index.js
@@ -1,65 +1,69 @@
 import { Utils } from "./utils.js"
+import { Stream } from "./stream.js"
+import { DbUtils } from "./dbutils.js"
+import { Constants } from "./constants.js"
 import { Log } from "./logger.js"
 const TAG = "index";
+
+const MIN_ROWS = 100;
+const ROWS = 500;
+const COLS = 50;
+const WIDTH = 100;
+const HEIGHT = 25;
+
 class Index {
 	constructor() {
-        let t = '<svg xmlns="http://www.w3.org/2000/svg">';
+        const canvas = document.getElementById('canvas');
+        canvas.width = COLS * WIDTH;
+        canvas.height = ROWS * HEIGHT;
+
+        this.ctx = canvas.getContext('2d');
+        this.ctx.textBaseline = 'top'; 
+        this.ctx.lineWidth = 0.5;
+        this.render();
+    }
+
+    async render() {
+        let creds = {
+            'db': 'test-generico',
+            'host': '127.0.0.1',
+            'port': '3306',
+            'user': 'server',
+            'pass': 'dev-server',
+        };
+
+        let sessionId = await DbUtils.login(creds);
+
+        let params = {
+            'session-id': sessionId,
+            query: `select * from \`bills-1\` limit ${ROWS}`
+        };
+
         let s = new Date();
 
-        for (let i = 0; i < 2000; i += 20) {
-            for (let j = 0; j < 2500; j += 50) {
-                t += this.getNodeXmlWithText('rect', {
-                    x: j,
-                    y: i,
-                    width: 50,
-                    height: 20,
-                    stroke:'black',
-                    strokeWidth:0.025,
-                    fill: '#fff'
-                }, 'some text')
-            }
-        }
+        let stream = new Stream(Constants.WS_URL + '/execute_ws?' + new URLSearchParams(params));
 
-        t += '</svg>';
-        let $body = document.querySelector('body');
-        $body.insertAdjacentHTML('beforeend', t);
+        let y = 0.5;
+
+        while (true) {
+            let row = await stream.get();
+
+            if (row.length == 1 && row[0] == "eos") {
+                break;
+            }
+
+            let x = 0.5;
+            for (let i = 1; i < row.length; i += 2) {
+                this.ctx.strokeRect(x, y, WIDTH, HEIGHT);
+                this.ctx.strokeText(row[i], x, y)
+                x += WIDTH;
+            }
+
+            y += HEIGHT;
+        }
 
         let e = new Date();
-        Log(TAG, e.getTime() - s.getTime());
-    }
-
-    getNode(n, v) {
-        n = document.createElementNS("http://www.w3.org/2000/svg", n);
-        for (var p in v)
-            n.setAttributeNS(null, p.replace(/[A-Z]/g, function(m, p, o, s) { return "-" + m.toLowerCase(); }), v[p]);
-        return n
-    }
-
-    getNodeXml(n, attrs) {
-        let t = `<${n} `;
-        for (let k in attrs) {
-            t += `${k}="${attrs[k]}" `;
-        }
-        t += `></${n}>`;
-        return t;
-    }
-
-    getNodeXmlWithText(n, attrs, s) {
-        let r = `<${n} `;
-        let t = `<foreignobject `;
-        let set = new Set(['x', 'y', 'height', 'width']);
-        for (let k in attrs) {
-            r += `${k}="${attrs[k]}" `;
-            if (set.has(k)) {
-                t += `${k}="${attrs[k]}" `;
-            }
-        }
-        r += `></${n}>`;
-
-        t += '><body xmlns="http://www.w3.org/1999/xhtml">';
-        t += `<div>${s}</div></body>'`;
-        t += `></foreignobject>`;
-        return r + t;
+        Log('index', e.getTime() - s.getTime());
     }
 }
 new Index()
