2e9253a8902d0811c4c3bc60e56321ece975c4bc
Pankaj Kargirwar <kargirwar@protonmail.com>
2021-06-16 16:43:19 +0530

    Testing AG with vue

---
+++ my-project/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <ag-grid-vue style="width: 500px; height: 500px;"
+        class="ag-theme-alpine"
+        :columnDefs="columnDefs"
+        :rowData="rowData">
+    </ag-grid-vue>
+</template>
+
+<script>
+    import { AgGridVue } from "ag-grid-vue3";
+
+    export default {
+        name: 'App',
+        data() {
+            return {
+                columnDefs: null,
+                rowData: null
+            }
+        },
+        components: {
+            AgGridVue
+        },
+        beforeMount() {
+            this.columnDefs = [
+                { field: 'make', resizable: true },
+                { field: 'model', resizable: true},
+                { field: 'price', resizable: true}
+            ];
+
+            this.rowData = [
+                { make: 'Toyota', model: 'Celica', price: 35000 },
+                { make: 'Ford', model: 'Mondeo', price: 32000 },
+                { make: 'Porsche', model: 'Boxter', price: 72000 }
+            ];
+        }
+    }
+</script>
+
+<style lang="scss">
+  @import "../node_modules/ag-grid-community/dist/styles/ag-grid.css";
+  @import "../node_modules/ag-grid-community/dist/styles/ag-theme-alpine.css";
+</style>
